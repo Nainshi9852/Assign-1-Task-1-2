77d951c021af5bf441fed7dacdf481d8d166dc4d
nainshisingh985@gmail.com <nainshisingh985@gmail.com>
2023-07-21 22:43:39 +0530

    Task-1-2

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task-1
+            Console.WriteLine("Hello World!!");
+
+            //Task-2
+            Console.WriteLine("Please enter your name:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello," + userName + "!");
+            Console.ReadKey();
+        }
+    }
+}
